cf8de014caad6b1536bf30aec5499e57df850fa5
Clevershelf009 <matthewski490@yahoo.co.uk>
2020-07-22 18:26:16 +0100

    Better Starting point for the display

--- src/Display.java
+++ src/Display.java
@@ -1,13 +1,19 @@
-import java.awt.*;
+import java.awt.Canvas;
+import java.awt.Graphics;
+import javax.swing.JFrame;
 
-class Display extends Frame {
-
-    Display() {
-        setSize(300,300);//frame size 300 width and 300 height
-        setVisible(true);//now frame will be visible, by default not visible
-    }
+class Display extends Canvas {
 
     public static void main(String[] args){
-        Display display = new Display();
+       JFrame frame = new JFrame("Boids Simulation");
+       Canvas canvas = new Display();
+       canvas.setSize(400, 400);
+       frame.add(canvas);
+       frame.pack();
+       frame.setVisible(true);
+    }
+
+    public void paint(Graphics g) {
+        g.fillOval(150, 100, 200, 200);
     }
 }
