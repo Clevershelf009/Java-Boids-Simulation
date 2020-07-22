95525ddd6ede930d0846574cd0ed35d0381fb06e
Clevershelf009 <matthewski490@yahoo.co.uk>
2020-07-22 18:02:57 +0100

    Further functionality added to Vector2D class. Fixed incorrect type in Vector2D constructor parameters.

--- src/Vector2D.java
+++ src/Vector2D.java
@@ -1,9 +1,11 @@
+import java.util.Vector;
+
 public class Vector2D {
 
     private double x;
     private double y;
 
-    Vector2D(int x, int y) {
+    Vector2D(double x, double y) {
         this.x = x;
         this.y = y;
     }
@@ -13,19 +15,39 @@ public class Vector2D {
         y += vector.getY();
     }
 
-    public void scale(float scaleFactor) {
-        x *= scaleFactor;
-        y *= scaleFactor;
+    public void subtract(Vector2D vector) {
+        x -= vector.getX();
+        y -= vector.getY();
+    }
+
+    public void multiply(double multiplier) {
+        x *= multiplier;
+        y *= multiplier;
+    }
+
+    public void divide(double divider) {
+        //TODO: Throw error when divider == 0
+        x /= divider;
+        y /= divider;
+
     }
 
     public double getMagnitude() {
         return Math.sqrt(x * x + y * y);
     }
 
+    public Vector2D getDelta(Vector2D otherVector) {
+        return new Vector2D(x - otherVector.getX(), y - otherVector.getY());
+    }
+
     public void normalise() {
         double magnitude = getMagnitude();
         x /= magnitude;
         y /= magnitude;
+    }
+
+    public double distanceTo(Vector2D otherVector) {
+        return Math.sqrt(Math.pow((x - otherVector.getX()), 2) + Math.pow((y - otherVector.getY()), 2));
     }
 
     public double getX() {
